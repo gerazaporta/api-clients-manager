bd7cbc5be1c57bfe5c688ae617d7d254523e8d48
Gerardo Zaporta <zaportagerardo@gmail.com>
2019-10-06 22:37:40 +0000

    latest changes

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-var createError = require('http-errors');
+// First import all dependencies
 var express = require('express');
+
+// Errors handler
+var createError = require('http-errors');
+ 
+
+// Map request into javascript objects
+const bodyParser = require('body-parser');
+// Cross 
+const cors = require('cors');
+// Helmet
+const helmet = require('helmet');
+
 var path = require('path');
+// Cookie parser
 var cookieParser = require('cookie-parser');
+
+// Logger
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Route files
+var apiRoutes = require('./routes/api');
+
+const jwt = require('./_helpers/jwt');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+// Logger
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// use JWT auth to secure the api
+app.use(jwt());
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/api', apiRoutes);
 
 module.exports = app;
